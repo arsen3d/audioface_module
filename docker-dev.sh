eb2ff17d7f767e2235c8edf9b2682db210e00b01
Arsen Yeremin <arsen3d@gmail.com>
2025-01-17 13:11:05 -0800

    Update

--- docker-dev.sh
+++ docker-dev.sh
@@ -1,3 +1,4 @@
+    #!/bin/bash
     if ! ipfs config show > /dev/null 2>&1; then
         ipfs init
         ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["https://webui.ipfs.io", "http://webui.ipfs.io.ipns.localhost:8080", "https://tauri.localhost", "http://localhost:5001"]'
@@ -6,5 +7,7 @@
     until curl -s http://127.0.0.1:5001/webui > /dev/null; do
         sleep 1
     done
+    
+    cd src
     watchmedo auto-restart --patterns=*.py --recursive -- python .
     # ipfs init && ipfs daemon & sleep 5 && python .
